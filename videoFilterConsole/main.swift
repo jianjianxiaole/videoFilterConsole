9de83a709a5bb10c18594e3b8d37e683251a26f3
wangjian <wangjian@wangjiandeMini.lan>
2015-07-08 23:28:34 +0800

    Initial Commit

---
+++ videoFilterConsole/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  videoFilterConsole
+//
+//  Created by wangjian on 15/7/8.
+//  Copyright (c) 2015年 wangjian. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
